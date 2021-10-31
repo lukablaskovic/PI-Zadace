b9ae536ef01b15458f786060f552acd2149b287d
lukablaskovic <lblaskovi@unipu.hr>
2021-10-31 15:22:01 +0100

    JS-405

---
+++ JS-405/JS-405.js
@@ -0,0 +1,26 @@
+let klijent = {
+  ime: "Petar",
+  prezime: "Starčević",
+  oib: "42812385012",
+  mjesto_prebivalista: {
+    adresa: "Vukovarska 12",
+    postanski_broj: "52100",
+    grad: "Pula",
+  },
+  sifre_posjecenih_trgovina: [10523, 88895, 44112, 22210],
+  kupljeni_artikli: [
+    {
+      naziv: "televizor",
+      marka: "Sony",
+      datum: "05.11.2020",
+      cijena: 4500,
+    },
+    {
+      naziv: "sušilo za kosu",
+      marka: "Philips",
+      datum: "02.02.2017",
+      cijena: 250,
+    },
+  ],
+};
+console.log(klijent);
